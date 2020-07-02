fd67cd196206c749d32f5248826699560458c60e
Daniel Montrallo Flickinger <daniel.m.flickinger@lmco.com>
2020-07-01 23:18:49 -0400

    fill out template with title page
    Implemented first iteration of title page. Set up footers.
    Created project files for the template, and the install script.
    Got working a basic version of the assignment template.

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-mkdir -p /usr/local/share/LaTeX_templates/RBE550_lecture/fig
+mkdir -p /usr/local/share/LaTeX_templates/RBE550_assignment/fig
 
-cp -f fig/*.png /usr/local/share/LaTeX_templates/RBE550_lecture/fig/
-
-cp -f template/RBE-550_S2021_lecture_template_preamble.tex /usr/local/share/LaTeX_templates/RBE550_lecture
-cp -f template/RBE-550_S2021_lecture_template_final_slide.tex /usr/local/share/LaTeX_templates/RBE550_lecture
+cp -f fig/*.png /usr/local/share/LaTeX_templates/RBE550_assignment/fig/
+cp -f template/*.tex /usr/local/share/LaTeX_templates/RBE550_assignment
