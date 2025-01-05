255aae1f4d444781a6f5c25b614fe202944d7c72
Daniel Montrallo Flickinger <daniel.m.flickinger@lmco.com>
2025-01-05 00:36:13 +0000

    update grading rubric
    attempt to include pie chart in grading rubric

--- .devcontainer/installDepsAndTemplates.sh
+++ .devcontainer/installDepsAndTemplates.sh
@@ -22,8 +22,30 @@ mkdir -p $(kpsewhich -var-value=TEXMFLOCAL)/tex/latex/RBEassignment/fig
 cp -f template/RBEassignment.cls $(kpsewhich -var-value=TEXMFLOCAL)/tex/latex/RBEassignment/
 cp -f template/fig/*.png $(kpsewhich -var-value=TEXMFLOCAL)/tex/latex/RBEassignment/fig/
 
+
+# Also get PGF-pie
+# ----------------
+
+CTAN_MIRROR=https://ctan.math.illinois.edu/graphics/pgf/contrib
+ZIP_FILE=pgf-pie.zip
+
+mkdir -p $(kpsewhich -var-value=TEXMFLOCAL)/tex/latex/generic
+
+curl ${CTAN_MIRROR}/${ZIP_FILE} -o $(kpsewhich -var-value=TEXMFLOCAL)/
+tex/latex/generic/${ZIP_FILE}
+
+unzip $(kpsewhich -var-value=TEXMFLOCAL)/tex/latex/generic/${ZIP_FILE} -x pgf-pie/demo/*
+
+mv pgf-pie $(kpsewhich -var-value=TEXMFLOCAL)/tex/latex/generic/
+rm -f $(kpsewhich -var-value=TEXMFLOCAL)/tex/latex/generic/${ZIP_FILE}
+
 # Register the RBE assignment class with texlive
 # ----------------------------------------------
 
 tlmgr conf texmf TEXMFLOCAL $(kpsewhich -var-value=TEXMFLOCAL)
 mktexlsr $(kpsewhich -var-value=TEXMFLOCAL)
+
+
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+dpkg-reconfigure --frontend=noninteractive locales
+update-locale LANG=en_US.UTF-8
